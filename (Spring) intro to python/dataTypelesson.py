40983eaf8b235ac4eb9580dfa8da257eb6800729
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-02-02 16:04:03 +0000

    2.2.24 Classwork

---
+++ (Spring) intro to python/dataTypelesson.py
@@ -0,0 +1,18 @@
+# Data Types
+
+#input- takes in data from a user.
+#input()
+
+# print- outputs data from your program.
+#print()
+
+#person = input('please enter your name:')
+#print('my name is ' + person)
+
+groceries='apples', 'oranges'
+
+#variables are placeholders/ containers for data (datatypes)
+Instructor = 'Ian Kimble'
+
+favoriteFood = input ('what is your favorite food? ')
+print('Your favorite food is'+ favoriteFood )
