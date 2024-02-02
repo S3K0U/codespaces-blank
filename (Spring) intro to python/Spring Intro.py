9e71490af5a010a1648eb749041638c884cbe69e
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-02-02 15:18:12 +0000

    work from 2.1.24

--- (Spring) intro to python/Spring Intro.py
+++ (Spring) intro to python/Spring Intro.py
@@ -1,7 +1,7 @@
 In your own words describe what a variable is? Has to relate to math
 
-What is the difference between a float and an integer?
+What is the difference between a float and an integer? a string of numbers
 
-In your own words, describe what the boolean data type is?
+In your own words, describe what the boolean data type is? true/false values
 
 Create four (4) variables and use the appropriate data type for each
