6c03f02f8d886c3475c74b6f2f6406721e62558a
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-02-21 15:30:54 +0000

    Async WORK

---
+++ warmupFeb12.py
@@ -0,0 +1,19 @@
+Q: What data type will this function return?
+
+A: int
+
+
+Q: What data type will this function return?
+
+A:
+
+
+Q: What does it mean when you encounter a TypeError?
+
+A:
+
+
+Q: What does it mean when you encounter SyntaxError?
+
+A:
+
