62d2a9ab7aabc2cd6256879862589a1bcb0cce66
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-02-26 15:39:18 +0000

    akeupQuiz1.

---
+++ activityFeb22.py
@@ -0,0 +1,13 @@
+# functions = sub-instructions inside of a program that only do one thing
+
+#functions have 2 parts
+#1. function definition- tells the computer what the function does
+
+def goodMorning():
+    print ('good morning') #function has one job/ instruction
+
+    #2. function call- tell the co,puter to run it
+
+    # function parameters and arguments
+
+    def giftCard
