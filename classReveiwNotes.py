01e5c2e726007820aba6e5e5f0cc503d24e81fee
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-03-07 16:19:04 +0000

    Reiveivewer

---
+++ classReveiwNotes.py
@@ -0,0 +1,24 @@
+#Programs - Instructions for computers
+#Programming Languages= different ways we can provide instructions to computers
+#Syntax= the the formatting rules of a language. in this case, a progamming language.
+
+#Additional note- when we are writing progams, all were doing is talking to the computer and giving it instructions to complete a task.
+
+#String-Allows progammers to write isolated characters that wont impact the programs; such as word
+
+#interger-a whole number/ non decimal number
+
+#float- a decimal number
+
+#boolan- a true or false value
+
+#Data type- are the alphabet for our programming
+
+#Variable= containers for data ( data are just data types)
+
+def myFunction():
+    print("Here are my instrunctions...")
+
+function myfunction(){
+    console.log("Here are my instructions...")
+}
