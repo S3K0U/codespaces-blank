104bc9a5ae9e11f508ad1d67afc5efd035efc7d9
S3K0U <158183808+S3K0U@users.noreply.github.com>
2024-04-30 14:46:34 +0000

    python quiz

---
+++ pythonspringquizreveiw2.py
@@ -0,0 +1,20 @@
+#what is a computer program?
+#-a set of instructions for computers to execute
+
+#WHAT IS SYNTAX?
+#the rules of a programming language.
+#ex. Spanish is language that has certain vocab rules that arent in other languages
+
+
+#an example of syntax (how to write) A function using python.
+def myfunction():
+    print('here are my instructions...')
+
+myfunction()
+
+
+#an example of syntax(how to write) a function in JavaScript.
+#function myFunction(){
+
+
+#Data type is a peice of information that the computer understands
